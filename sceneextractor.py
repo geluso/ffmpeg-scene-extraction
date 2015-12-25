1590242c25c107e04671f10787719e8b6bba7844
Steve Geluso <stevegeluso@gmail.com>
2015-12-24 23:11:33 -0800

    printing movie length. refactoring creating methods

--- sceneextractor.py
+++ sceneextractor.py
@@ -2,8 +2,11 @@
 
 import datetime
 import argparse
-from subprocess import call
+from subprocess import call, check_output
 from uuid import uuid1
+
+# ffprobe -i clip.dv -show_format 2>&1 | grep 'duration='
+duration_command = "ffprobe -i %s -show_format 2>&1 | grep 'duration='"
 
 # ffprobe -show_frames -of compact=p=0 -f lavfi "movie=clip-135-02-05\ 05;27;15.dv,select=gt(scene\,.4)" > scenes.txt
 probe_command = "ffprobe -show_frames -of compact=p=0 -f lavfi \"movie=%s,select=gt(scene\,.4)\" > scenes.txt"
@@ -12,12 +15,10 @@ probe_command = "ffprobe -show_frames -of compact=p=0 -f lavfi \"movie=%s,select
 segment_command = "ffmpeg -i %s -f segment -segment_times LIST_OF_TIMES -c copy -map 0:0 %s/%04d.dv"
 
 # TODO:
-# - detect and print the length of the video
 # - suppress ffprobe output
 # - run ffprobe on another thread
 # - create a second thread to repeatedly poll the file scenes file
 # - print the progress of the number of scenes detected and the current time in the movie.
-
 
 now = datetime.datetime.now()
 default_out_name = "scenes_%d_%02d_%02d_%02d.mp4" % (now.year, now.hour, now.minute, now.second)
@@ -27,19 +28,45 @@ parser = argparse.ArgumentParser(description="This takes one large video file an
 parser.add_argument("in_file", type=str, help="Name of the input file.")
 parser.add_argument("out_dir", nargs="?", type=str, default=default_out_name, help="Name of the output directory to be created.")
 
-args = parser.parse_args()
+def print_movie_length(filename):
+  cmd = duration_command % (filename)
+  duration = check_output(cmd, shell=True)
+  duration = duration.split("duration=")[1]
 
-# probe the movie and create a text file containing timestamps of scene changes
-print "Probing movie for scene changes..."
-cmd = probe_command % (args.in_file)
-call(cmd, shell=True)
+  # eliminate fractions of a second
+  duration = duration.split(".")[0]
+  duration = int(duration)
 
-print "Probing complete. N scenes found."
-print "Processing scene timestamps..."
+  # calculate hours, minutes, seconds
+  minutes = duration / 60
+  if (minutes > 60):
+    hours = minutes / 60
+    minutes = minutes % 60
+  seconds = duration % 60
 
-print "Creating directory for scenes..."
-# create out directory for clips
-call("mkdir %s" % args.out_dir, shell=True)
+  print "Movie is %02d:%02d:%02d long" % (hours, minutes, seconds)
 
-print "Extracting scenes..."
+def detect_scenes(filename):
+  # probe the movie and create a text file containing timestamps of scene changes
+  print "Probing movie for scene changes..."
+  cmd = probe_command % (filename)
+  call(cmd, shell=True)
 
+  print "Probing complete. N scenes found."
+  print "Processing scene timestamps..."
+
+def extract_scenes():
+  print "Creating directory for scenes..."
+  # create out directory for clips
+  call("mkdir %s" % args.out_dir, shell=True)
+
+  print "Extracting scenes..."
+
+def main():
+  args = parser.parse_args()
+  filename = args.in_file.replace(" ", "\\ ")
+
+  print_movie_length(filename)
+  detect_scenes(filename)
+
+main()
