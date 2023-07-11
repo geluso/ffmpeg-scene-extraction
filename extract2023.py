6463f83eb208a38ba1f8eea34854a8dbc9f1618f
Steve Geluso <steve.geluso@convoy.com>
2023-07-10 22:05:42 -0700

    especially adding -ac 2 option to extract command

--- extract2023.py
+++ extract2023.py
@@ -1,81 +1,129 @@
 from subprocess import call, check_output
+import os
 import sys
+import time
+
+import gather_srt
+
+EXPECTED_EXTENSIONS = ["avi", "mp4", "mkv", "mov"]
 
 # ffprobe -i clip.dv -show_format 2>&1 | grep 'duration='
-duration_command = "ffprobe -i %s -show_format 2>&1 | grep 'duration='"
+duration_command = "ffprobe -i '%s' -show_format 2>&1 | grep 'duration='"
 
 # ffprobe -show_frames -of compact=p=0 -f lavfi "movie=clip-135-02-05\ 05;27;15.dv,select=gt(scene\,.4)" > scenes.txt
-probe_command = "ffprobe -show_frames -of compact=p=0 -f lavfi \"movie=%s,select=gt(scene\,.4)\" > %s 2>&1"
+probe_command = "ffprobe -show_frames -of compact=p=0 -f lavfi \"movie='%s',select=gt(scene\,.4)\" > '%s' 2>&1"
 
 # ffmpeg -i clip.dv -f segment -segment_times 7,20,47 -c copy -map 0:0 scenes/%04d.dv"""
 #extract_command = "ffmpeg -i %s -f segment -segment_times %s -c copy -map 0:0 %s/%%04d.avi 2>&1"
-extract_command = "ffmpeg -i %s -segment_times %s -reset_timestamps 1 -f segment %s/%%04d.mp4 2>&1"
+extract_command = "ffmpeg -i '%s' -ac 2 -segment_times '%s' -reset_timestamps 1 -f segment '%s/%%04d.mp4' 2>&1"
 
-episode = sys.argv[1]
-filename = "/Users/geluso/Torrents/I.Think.You.Should.Leave.with.Tim.Robinson.%s.XviD-AFG/I.Think.You.Should.Leave.with.Tim.Robinson.%s.XviD-AFG.avi" % (episode, episode)
-output = "output.txt"
+create_json_structure_command = 'tree -J > structure.json'
 
-video_output_dir = "%s/scenes" % (episode)
-audio_output_dir = "%s/audio" % (episode)
-text_output_dir = "%s/text" % (episode)
+def main():
+    with open("progress", "a") as progress_file:
+        for root, dirs, filenames in os.walk("."):
+            msg = "Found %d files." % len(filenames)
+            print(msg)
+            progress_file.writelines(msg)
+            progress_file.flush()
 
-video_to_audio_command = "ffmpeg -i %s -ar 16000 %s"
+            for filename in filenames:
+                extension = filename.split(".")[-1]
+                if extension in EXPECTED_EXTENSIONS:
+                    process_file(filename, progress_file)
+                else:
+                    print("Skipping file with non-expected extension:", filename)
+        do_cmd(create_json_structure_command)
 
-#audio_to_text_command = "/Users/geluso/Code/whisper.cpp/main -p 8 -t 10 -pp -m /Users/geluso/Code/whisper.cpp/models/ggml-large.bin -f %s -osrt -of %s"
-#audio_to_text_command = "/Users/geluso/Code/whisper.cpp/main -m /Users/geluso/Code/whisper.cpp/models/ggml-large.bin -f %s -osrt -of %s"
-#audio_to_text_command = "/Users/geluso/Code/whisper.cpp/main -p 4 -m /Users/geluso/Code/whisper.cpp/models/ggml-large.bin -f %s -osrt -of %s"
-audio_to_text_command = "/Users/geluso/Code/whisper.cpp/main -m /Users/geluso/Code/whisper.cpp/models/ggml-large.bin -f %s -osrt -of %s"
-
-#cmd = "rm -rf %s" % (video_output_dir)
-#call(cmd, shell=True)
-#cmd = "rm -rf %s" % (audio_output_dir)
-#call(cmd, shell=True)
-#cmd = "rm -rf %s" % (text_output_dir)
-#call(cmd, shell=True)
-
-cmd = "mkdir -p %s" % (video_output_dir)
-call(cmd, shell=True)
-cmd = "mkdir -p %s" % (audio_output_dir)
-call(cmd, shell=True)
-cmd = "mkdir -p %s" % (text_output_dir)
-call(cmd, shell=True)
-
-cmd = duration_command % (filename)
-duration_result = call(cmd, shell=True)
-print("duration:", duration_result)
-
-cmd = probe_command % (filename, output)
-probe_result = call(cmd, shell=True)
-print("probe:", probe_result)
-
-probe_lines = open(output).readlines()
-timestamps = []
-for line in probe_lines:
-    if "best_effort_timestamp_time" in line:
-        line = line.split("best_effort_timestamp_time=")
-        line = line[1]
-        line = line.split("|")
-        line = line[0]
-        line = str(int(float(line)))
-        timestamps.append(line)
-
-print("Found", len(timestamps), "scenes")
-print("Extracting...")
-
-all_segment_times = ",".join(timestamps)
-cmd = extract_command % (filename, all_segment_times, video_output_dir)
-extract_result = call(cmd, shell=True)
-
-for i in range(len(timestamps)):
-    video_file = "%s/%04d.mp4" % (video_output_dir, i)
-    audio_file = "%s/%04d.wav" % (audio_output_dir, i)
-    text_file = "%s/%04d" % (text_output_dir, i)
-
-    cmd = video_to_audio_command % (video_file, audio_file)
-    print("converting video to audio:", cmd)
-    video_to_audio_result = call(cmd, shell=True)
-
-    cmd = audio_to_text_command % (audio_file, text_file)
-    print("converting audio to text")
+def do_cmd(cmd, is_checking_output=False, is_breakpoint=False):
     print(cmd)
-    audio_to_text_result = call(cmd, shell=True)
+    if is_breakpoint:
+        breakpoint()
+    if not is_checking_output:
+        call(cmd, shell=True)
+        return
+    result = check_output(cmd, shell=True)
+    print(result)
+    print()
+    return result
+
+
+def make_dir(dir_name):
+    cmd = "mkdir -p '%s'" % (dir_name)
+    return do_cmd(cmd)
+
+def get_duration(filename):
+    cmd = duration_command % (filename)
+    return do_cmd(cmd, True)
+
+def probe_scenes(filename, output):
+    cmd = probe_command % (filename, output)
+    return do_cmd(cmd)
+
+def process_file(filename, progress_file):
+    print("Processing", filename)
+
+    print("with spaces", filename)
+    filename_no_extension = os.path.splitext(filename)[0]
+    filename_no_extension = filename_no_extension.replace(" ", "_")
+    print(" w/o spaces", filename_no_extension)
+
+    probed_scene_timestamps_file = "%s.txt" % filename_no_extension
+    video_output_dir = "%s/scenes" % (filename_no_extension)
+    audio_output_dir = "%s/audio" % (filename_no_extension)
+    text_output_dir = "%s/text" % (filename_no_extension)
+
+    video_to_audio_command = "ffmpeg -i %s -ar 16000 %s"
+
+    audio_to_text_command = "/Users/geluso/Code/whisper.cpp/main -m /Users/geluso/Code/whisper.cpp/models/ggml-base.en.bin -f '%s' -osrt -of '%s'"
+
+    #cmd = "rm -rf %s" % (video_output_dir)
+    #call(cmd, shell=True)
+    #cmd = "rm -rf %s" % (audio_output_dir)
+    #call(cmd, shell=True)
+    #cmd = "rm -rf %s" % (text_output_dir)
+    #call(cmd, shell=True)
+
+    make_dir(video_output_dir)
+    make_dir(audio_output_dir)
+    make_dir(text_output_dir)
+
+    get_duration(filename)
+    probe_scenes(filename, probed_scene_timestamps_file)
+
+    probe_lines = open(probed_scene_timestamps_file).readlines()
+    timestamps = []
+    for line in probe_lines:
+        if "best_effort_timestamp_time" in line:
+            line = line.split("best_effort_timestamp_time=")
+            line = line[1]
+            line = line.split("|")
+            line = line[0]
+            line = str(int(float(line)))
+            timestamps.append(line)
+
+    print("Found", len(timestamps), "scenes")
+    print("Extracting...")
+
+    all_segment_times = ",".join(timestamps)
+    cmd = extract_command % (filename, all_segment_times, video_output_dir)
+    do_cmd(cmd)
+
+    for i in range(len(timestamps)):
+        video_file = "%s/%04d.mp4" % (video_output_dir, i)
+        audio_file = "%s/%04d.wav" % (audio_output_dir, i)
+        text_file = "%s/%04d" % (text_output_dir, i)
+
+        cmd = video_to_audio_command % (video_file, audio_file)
+        print("converting video to audio:", cmd)
+        call(cmd, shell=True)
+
+        cmd = audio_to_text_command % (audio_file, text_file)
+        print("converting audio to text")
+        print(cmd)
+        call(cmd, shell=True)
+
+    gather_srt.create_json_file(filename_no_extension)
+
+
+main()
--- gather_srt.py
+++ gather_srt.py
@@ -2,42 +2,42 @@ from subprocess import call, check_output
 import json
 import sys
 
-episode = sys.argv[1]
-text_dir = "%s/text" % episode
+def create_json_file(video_dir):
+    text_dir = "%s/text" % video_dir
 
-list_text_files = "ls -1 %s" % text_dir
-cmd = list_text_files
+    list_text_files = "ls -1 %s" % text_dir
+    cmd = list_text_files
 
-text_files = check_output(cmd, shell=True)
+    text_files = check_output(cmd, shell=True)
 
-data = {}
-for file in text_files.split():
-    clip = file.decode("utf-8")
-    filename = text_dir + '/' + clip
-    ff = open(filename)
-    lines = ff.readlines()
-    clip_json = []
-    
-    count = 0
-    for line in lines:
-        if count % 4 == 0:
-            pass
-        elif count % 4 == 1:
-            timestamp = line.strip()
-        elif count % 4 == 2:
-            text = line.strip()
-            clip_json.append({
-                "timestamp": timestamp,
-                "text": text
-            })
-        elif count % 4 == 3:
-            pass
-        count += 1
-    data[clip] = clip_json
-print(data)
+    data = {}
+    for file in text_files.split():
+        clip = file.decode("utf-8")
+        filename = text_dir + '/' + clip
+        ff = open(filename)
+        lines = ff.readlines()
+        clip_json = []
 
-jj = json.dumps(data)
-json_filename = "%s/%s.json" % (episode, episode)
-ff = open(json_filename, mode="w")
-ff.write(jj)
-ff.close()
+        count = 0
+        for line in lines:
+            if count % 4 == 0:
+                pass
+            elif count % 4 == 1:
+                timestamp = line.strip()
+            elif count % 4 == 2:
+                text = line.strip()
+                clip_json.append({
+                    "timestamp": timestamp,
+                    "text": text
+                })
+            elif count % 4 == 3:
+                pass
+            count += 1
+        data[clip] = clip_json
+    print(data)
+
+    jj = json.dumps(data)
+    json_filename = "%s/%s.json" % (video_dir, video_dir)
+    ff = open(json_filename, mode="w")
+    ff.write(jj)
+    ff.close()
